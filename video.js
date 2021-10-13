fb371c026a95c161957bac5135808014de31fcf0
Efty <efty131331@gmail,com>
2021-10-13 12:24:36 +0600

    video player js problem fixed

--- video.js
+++ video.js
@@ -1,11 +1,13 @@
 const videoPlayer = document.getElementById('videoPlayer');
 const myVideo = document.getElementById('myVideo');
 
-function stopVideo(){
+
+function stopVideo() {
+    myVideo.pause()
     videoPlayer.style.display = 'none';
 }
 
-function playVideo(file){
+function playVideo(file) {
     myVideo.src = file;
     videoPlayer.style.display = 'block';
 }
